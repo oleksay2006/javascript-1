d6820450e6f0e2968c50b7b1c2f529745c87b47b
oleksay2006 <oleksay2006@gmail.com>
2021-05-05 23:50:25 +0300

    s
    s

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let clicks = 0;
+let div = document.createElement('div');
+let button = document.querySelector('container-btn');
+div.classList.add("container");
 function hide() {
 	clicks += 1;
 	document.getElementById("container").style.display = 'none';
@@ -7,8 +10,15 @@ function hide() {
 	}
 }
 function hide_2() {
+	clicks += 1;
 	let elem = document.querySelector('#container');
-	elem.parentElement.removeChild(elem);
+	if (clicks % 2 != 0) {
+		elem.parentElement.removeChild(elem);
+	}
+	
+	if (clicks % 2 == 0) {
+		document.body.appendChild(div);
+	}
 }
 function hide_3() {
 	clicks += 1;
@@ -17,4 +27,6 @@ function hide_3() {
 		document.getElementById("container").classList.remove("hidden");
 	}
 }
-document.getElementsByClassName('container').classList.remove("hidden")
+// for (let i=0;i < 5; i++) {
+document.getElementsByClassName('container')[0].classList.remove("hidden")
+// }
